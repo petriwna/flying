064049aed70ceb8cc6bea47c328693b9b0d3616e
petriwna <petriwna43@gmail.com>
2025-04-12 02:35:22 +0300

    fix

---
+++ nuxt.config.js
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+  app: {
+    baseURL: '/flying/', // Обов'язково для GitHub Pages (ім'я репозиторію)
+  },
+  head: {
+    title: 'Nuxt.js starter for CSB',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Official Nuxt.js starter for CodeSandBox'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  components: true,
+  css: ['@/assets/styles/main.scss'],
+  plugins: [],
+})
